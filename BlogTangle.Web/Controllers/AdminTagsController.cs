5913ff3f75fdadbd7989c32b7cc282651165c411
Emirhan Dağaşan <emirhan.d.dagasan@gmail.com>
2024-08-16 02:44:01 +0300

    Added Add Page

---
+++ BlogTangle.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,43 @@
+﻿using BlogTangle.Web.Data;
+using BlogTangle.Web.Models.Domain;
+using BlogTangle.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogTangle.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public AdminTagsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTagViewModel addTagViewModel)
+        {
+            // Mapping AddTagViewModel to Tag domain model
+            Tag tag = new Tag
+            {
+                Name = addTagViewModel.Name,
+                DisplayName = addTagViewModel.DisplayName
+            };
+
+            _db.Tags.Add(tag);
+            _db.SaveChanges();
+
+            return View("Add");
+        }
+    }
+
+
+
+}
---
+++ BlogTangle.Web/Models/ViewModels/AddTagViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogTangle.Web.Models.ViewModels
+{
+    public class AddTagViewModel
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
