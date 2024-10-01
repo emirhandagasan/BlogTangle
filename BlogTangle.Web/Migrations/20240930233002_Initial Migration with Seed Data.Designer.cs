96d60daedebee9c9b6916bcd750d2a0afd8087c7
Emirhan Dağaşan <emirhan.d.dagasan@gmail.com>
2024-10-01 04:07:42 +0300

    Add Seed Data

--- BlogTangle.Web/Migrations/20240930233002_Initial Migration with Seed Data.Designer.cs
+++ BlogTangle.Web/Migrations/20240930233002_Initial Migration with Seed Data.Designer.cs
@@ -4,6 +4,7 @@ using BlogTangle.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogTangle.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240930233002_Initial Migration with Seed Data")]
+    partial class InitialMigrationwithSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
